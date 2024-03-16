56a55a204f3923aae495b5201606638e4018d534
Thejaswi A <thejaswi@arista.com>
2024-03-16 23:02:49 +0530

    Contact card added

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -8,9 +8,33 @@ const Contact = () => {
     return (
         
         <div id='Contact' className='contact'>
-            <div className='wip'>
+            {/* <div className='wip'>
                     <h1 className='wiph'>UNDER CONSTRUCTION</h1>
+                </div> */}
+            <div id="container">
+            
+            <form action="#" method="post" id="contact_form">
+                <div class="name">
+                <label for="name"></label>
+                <input type="text" placeholder="Name" name="name" id="name_input" required></input>
                 </div>
+                <div class="email">
+                <label for="email"></label>
+                <input type="email" placeholder="e-mail" name="email" id="email_input" required></input>
+                </div>
+                <div class="subject">
+                    <label for="subject"></label>
+                <input type="text" placeholder="Subject" name="name" id="name_input" required></input>
+                </div>
+                <div class="message">
+                <label for="message"></label>
+                <textarea name="message" placeholder="Message" id="message_input" cols="30" rows="5" required></textarea>
+                </div>
+                <div class="submit">
+                <input type="submit" value="Send Message" id="form_button" />
+                </div>
+            </form>
+            </div>
         </div>
     )
 }
