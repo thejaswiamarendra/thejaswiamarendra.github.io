f61f1d568569cd5d97418155c5bcfc22862fb95c
Thejaswi A <thejaswia03@gmail.com>
2025-07-15 23:05:43 +0530

    Add Hashrouter

--- src/App.js
+++ src/App.js
@@ -5,61 +5,8 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Work from './pages/Work/Work';
 import Personal from './pages/Personal/Personal';
-
-import ghatInBenaras from './assets/ghatInBenaras.png';
-import kujukushimashimabara from './assets/kujukushimashimabara.png';
-import moonOverToyamaPlain from './assets/moonOverToyamaPlain.png'
-import shinobazuPondAtNight from './assets/shinobazuPondAtNight.png'
-import shirahamaEngetsuIsland from './assets/shirahamaEngetsuIsland.png'
-import tajMahalUnderMoonlight from './assets/tajMahalUnderMoonlight.png'
-import basishinjiBeach from './assets/basishinjiBeach.png'
+import { backgroundImages } from './data';
 import './App.css';
-
-
-const backgroundImages = [
-    {
-        "title": "Ghat in Benaras (Benaresu no gatto)",
-        "src": ghatInBenaras,
-        "artist": "Hiroshi Yoshida",
-        "year": "1931"
-    },
-    {
-        "title": "Kujukushima Shimabara Island",
-        "src": kujukushimashimabara,
-        "artist": "Kawase Hasui",
-        "year": "1922"
-    },
-    {
-        "title": "Winter Moon over Toyama Plain (Fuyu no tsuki (Toyamagahara))",
-        "src": moonOverToyamaPlain,
-        "artist": "Kawase Hasui",
-        "year": "1931"
-    },
-    {
-        "title": "Shinobazu Pond at Night (Yoru no chihan (Shinobazu no ike))",
-        "src": shinobazuPondAtNight,
-        "artist": "Kawase Hasui",
-        "year": "1932"
-    },
-    {
-        "title": "Engetsu Island, Shirahama (Shirahama Engetsuto)",
-        "src": shirahamaEngetsuIsland,
-        "artist": "Kawase Hasui",
-        "year": "1932"
-    },
-    {
-        "title": "Taj Mahal under Moonlight (Tsukiyo no Taji Maharu)",
-        "src": tajMahalUnderMoonlight,
-        "artist": "Hiroshi Yoshida",
-        "year": "1931"
-    },
-    {   
-        "title": "Basishinji Beach, Iyo (Iyo Baishinji no hama)",
-        "src": basishinjiBeach,
-        "artist": "Kawase Hasui",
-        "year": "1950"
-    }
-]
 
 function App() {
   // Select three unique images for the pages
